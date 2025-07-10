20647b4c460694d139c20360680443132e26f6c2
galoistom <tomliuo417@outlook.com>
2025-07-10 18:34:46 +0800

    update

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -3,7 +3,10 @@ local map = vim.keymap.set
 map('n', '<leader>w', '<C-w>', { desc = 'move focous' })
 map('n', '<leader>e', '<cmd>NERDTreeToggle %:p:h<CR>', { desc = 'nerdtree' })
 map('n', '<leader>q', '<cmd>wq<CR>', { desc = 'quit' })
-map('n', '<leader>E', '<cmd>Explore<CR>')
+map('n', '<leader>E', function()
+  vim.cmd 'tabnew'
+  vim.cmd 'Explore'
+end)
 map('n', '<Tab>', '<cmd>bn<CR>', { desc = 'next buffer' })
 map('n', '<S-Tab>', '<cmd>bp<CR>', { desc = 'previous buffer' })
 map('n', '<leader>t', '<cmd>belowright term<CR>', { desc = 'terminal' })
