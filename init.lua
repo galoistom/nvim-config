6807cdbe665008b1ae427b93db5ff398f6fca526
galoistom <tomliuo417@outlook.com>
2025-08-29 09:41:57 +0800

    updates

--- init.lua
+++ init.lua
@@ -986,7 +986,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l:%-2v | ' .. os.date '%H:%M'
       end
 
       -- ... and there is more!
