dac485f27508c4b79b3c918bdb0524f23988e748
galoistom <tomliuo417@outlook.com>
2025-09-12 11:11:28 +0800

    edit color

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -101,8 +101,8 @@ local colors = {
         color = function()
           -- auto change color according to neovims mode
           local mode_color = {
-            n = colors.red,
-            i = colors.green,
+            n = colors.green,
+            i = colors.red,
             v = colors.blue,
             [''] = colors.blue,
             V = colors.blue,
