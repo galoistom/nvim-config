ebbe0faa46a2b4b036a3ed269291e86082277c01
galoistom <tomliuo417@outlook.com>
2025-10-05 12:49:17 +0800

    change shortcuts

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -5,10 +5,10 @@ map('n', '<leader>w', '<C-w>', { desc = 'move focous' })
 map('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true, desc = 'nerdtree' })
 map('n', '<leader>Q', '<cmd>wq<CR>', { desc = 'save and quit' })
 map('n', '<leader>q', '<cmd>q<CR>', { desc = 'quit' })
-map('n', '<leader>E', function()
-  vim.cmd 'tabnew'
-  vim.cmd 'Explore'
-end, { desc = 'Explore' })
+--map('n', '<leader>E', function()
+--  vim.cmd 'tabnew'
+--  vim.cmd 'Explore'
+--end, { desc = 'Explore' })
 map('n', '<Tab>', '<cmd>bn<CR>', { desc = 'next buffer' })
 map('n', '<S-Tab>', '<cmd>bp<CR>', { desc = 'previous buffer' })
 map('n', '<leader><Space>', '<cmd>belowright term<CR>', { desc = 'terminal' })
